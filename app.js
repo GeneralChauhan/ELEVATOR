cb745bab95ae2183fd8fba2fbe61262af55e8801
God <abhinabpratap1311@gmail.com>
2022-12-15 22:30:13 +0530

    optimizations for responsive design

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const el = document.getElementById("elevator");
+const floor1 = document.getElementById("floor1");
+const floor2 = document.getElementById("floor2");
+const floor3 = document.getElementById("floor3");
+const upBtn = document.getElementById("up");
+const downBtn = document.getElementById("down");
+
 // document.getElementById("elevator").style.top = "227px";
 // console.log(floor);
 let topPos = 0;
+let floor1top= floor1.offsetTop;
+let floor2top= floor2.offsetTop;
+let floor3top= floor3.offsetTop;
 
 const elUp = () => {
-  let floor = el.offsetTop;
-  if (floor === 20) {
+  let elevatorTop= el.offsetTop;
+  if (elevatorTop===  floor3top) {
     alert("You are already on the top floor");
-    topPos = 20;
-  } else if (floor === 227) {
-    topPos = 20;
-  } else if (floor === 433) {
-    topPos = 227;
+    topPos = floor3top;
+  } else if (elevatorTop=== floor2top) {
+    topPos = floor2top;
+  } else if (elevatorTop=== floor1top) {
+    topPos = floor1top;
   }
   document.getElementById("elevator").style.top = topPos + "px";
 };
 
 const elDown = () => {
-  let floor = el.offsetTop;
-  if (floor === 20) {
+  let elevatorTop= el.offsetTop;
+  if (elevatorTop=== floor3top) {
     topPos = 227;
-  } else if (floor === 227) {
+  } else if (elevatorTop=== floor2top) {
     topPos = 433;
-  } else if (floor === 433) {
+  } else if (elevatorTop=== floor1top) {
     alert("You are already on the Ground Floor");
     topPos = 433;
   }
